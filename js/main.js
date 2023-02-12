068efe8679ab087623567ea8b6f17c53cf7201f6
NicolasBQ <nicolasbquintero@gmail.com>
2023-02-12 16:11:42 -0500

    Test #13

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-import { getForm } from "form.js";
-import { handler } from "functionalities/handler.js";
-import { changeMeasurement } from "measurement.js";
+// import { getForm } from "form.js";
+// import { handler } from "functionalities/handler.js";
+// import { changeMeasurement } from "measurement.js";
 import { mediaSize } from "mediaQueries.js";
 
 const appInit = () => {
     alert('SSi');   
     mediaSize();
-    handler('Bogota');
-    getForm();
-    changeMeasurement();
+    // handler('Bogota');
+    // getForm();
+    // changeMeasurement();
 }
 
 
