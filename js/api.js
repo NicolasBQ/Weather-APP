4b68e71284730a132f2fbcf0a52d9cf88ad00cc7
NicolasBQ <nicolasbquintero@gmail.com>
2023-02-12 18:32:11 -0500

    Test

---
+++ js/api.js
@@ -0,0 +1,3 @@
+const API = '137d39d013f2ec6ecc8b190c1b54f9a2';
+
+export { API }
--- js/handler.js
+++ js/handler.js
@@ -7,17 +7,17 @@ const handler = async (location) => {
     showLoader();
     const geoCity = await geo(location);
     if(geoCity) {
-        // const cityLat = geoCity.lat;
-        // const cityLon = geoCity.lon;
-        // const city = geoCity.city;
-        // const currentWeather = await current(cityLat, cityLon);
-        // localStorage.setItem('city', city);
-        // localStorage.setItem('currentResponse', JSON.stringify(currentWeather.currentResponse));
-        // currentDisplay(city, currentWeather.currentResponse);
-        // hideLoader();
+        const cityLat = geoCity.lat;
+        const cityLon = geoCity.lon;
+        const city = geoCity.city;
+        const currentWeather = await current(cityLat, cityLon);
+        localStorage.setItem('city', city);
+        localStorage.setItem('currentResponse', JSON.stringify(currentWeather.currentResponse));
+        currentDisplay(city, currentWeather.currentResponse);
+        hideLoader();
     } else {
-        // handleError();
-        // hideLoader();
+        handleError();
+        hideLoader();
     }
 }
 
