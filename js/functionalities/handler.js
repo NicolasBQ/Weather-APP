447d7a45acb120abe48f3ce04a1e8d94b56a355c
NicolasBQ <nicolasbquintero@gmail.com>
2023-02-12 18:07:33 -0500

    Test #24

--- js/functionalities/handler.js
+++ js/functionalities/handler.js
@@ -1,37 +1,39 @@
-import { geo } from "../data/geo.js";
-import { current } from "../data/current.js";
-import { currentDisplay, handleError } from "./currentWeather.js";
+// import { geo } from "../data/geo.js";
+// import { current } from "../data/current.js";
+// import { currentDisplay, handleError } from "./currentWeather.js";
 
-const handler = async (location) => {
-    showLoader();
-    const geoCity = await geo(location);
-    if(geoCity) {
-        const cityLat = geoCity.lat;
-        const cityLon = geoCity.lon;
-        const city = geoCity.city;
-        const currentWeather = await current(cityLat, cityLon);
-        localStorage.setItem('city', city);
-        localStorage.setItem('currentResponse', JSON.stringify(currentWeather.currentResponse));
-        currentDisplay(city, currentWeather.currentResponse);
-        hideLoader();
-    } else {
-        handleError();
-        hideLoader();
-    }
+// const handler = async (location) => {
+//     showLoader();
+//     const geoCity = await geo(location);
+//     if(geoCity) {
+//         const cityLat = geoCity.lat;
+//         const cityLon = geoCity.lon;
+//         const city = geoCity.city;
+//         const currentWeather = await current(cityLat, cityLon);
+//         localStorage.setItem('city', city);
+//         localStorage.setItem('currentResponse', JSON.stringify(currentWeather.currentResponse));
+//         currentDisplay(city, currentWeather.currentResponse);
+//         hideLoader();
+//     } else {
+//         handleError();
+//         hideLoader();
+//     }
+// }
+
+// const showLoader = () => {
+//     const loader = document.getElementById('loader');
+//     loader.classList.add('show')
+// }
+
+// const hideLoader = () => {
+//     const loader = document.getElementById('loader');
+//     loader.classList.remove('show')
+// }
+
+
+const handler = () => {
+    alert('Hello from alert');
 }
-
-const showLoader = () => {
-    const loader = document.getElementById('loader');
-    loader.classList.add('show')
-}
-
-const hideLoader = () => {
-    const loader = document.getElementById('loader');
-    loader.classList.remove('show')
-}
-
-
-
 
 
 export { handler }
