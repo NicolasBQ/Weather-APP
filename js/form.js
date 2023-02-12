d0e9ab7deea525187588ac980aa091a4ebf34319
NicolasBQ <nicolasbquintero@gmail.com>
2023-02-12 18:06:26 -0500

    Test #23

--- js/form.js
+++ js/form.js
@@ -1,15 +1,15 @@
-// import { handler } from "./functionalities/handler";
+import { handler } from "./functionalities/handler.js";
 
 const getForm = () => {
     const form = document.querySelector('[data-search-form]');
     form.addEventListener('submit', formHandler)
 }
 
-// const formHandler = async (e) => {
-//     e.preventDefault();
-//     const location = document.querySelector('[data-search-input]').value;
-//     localStorage.setItem('city', location);
-//     handler(location);
-// }
+const formHandler = async (e) => {
+    e.preventDefault();
+    const location = document.querySelector('[data-search-input]').value;
+    localStorage.setItem('city', location);
+    handler(location);
+}
 
 export { getForm }
